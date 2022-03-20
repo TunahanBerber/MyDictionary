b41b52c28502f7da6e4752bd4ec805bf13e67ec7
Tunahan Berber <tunahanberber123@gmail.com>
2022-03-21 01:18:19 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using MyDictionary;
+
+namespace IDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> persons1 = new Dictionary<int, string>();
+            persons1.Add(18, "Melek");
+            persons1.Add(23, "Tunahan");
+            Console.WriteLine(persons1.Count);
+
+            MyDictionary<int, string> persons2 = new MyDictionary<int, string>();
+            persons1.Add(26, "Aybüke");
+            persons2.Add(30, "Deniz");
+            Console.WriteLine(persons2.Count);
+
+
+        }
+    }
+}
